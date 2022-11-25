096bcd7de13ffcf783f2f0e62a174630439adfa8
Konstantinos Gallis <kongallis@gmail.com>
2022-11-25 20:59:25 +0200

    Move list of users to a separate component UsersList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import { useState, useEffect } from "react";
-import Card from "./components/Card"
+import UsersList from "./components/UsersList"
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    async function getUsers() {
-      const result = await fetch('https://random-data-api.com/api/v2/users?size=25')
-        .then(response => response.json())
-        .then(data => setUsers(data))
-    }
-
-    getUsers();
-  }, []);
-
   return (
-    <ul className="users-list">
-      {
-        users.map(user => <li key={user.id}><Card user={user} /></li>)
-      }
-    </ul>
+    <UsersList />
   )
 }
 
---
+++ src/components/UsersList.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react"
+import Card from "./Card"
+
+const UserList = () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    async function getUsers() {
+      const result = await fetch('https://random-data-api.com/api/v2/users?size=25')
+        .then(response => response.json())
+        .then(data => setUsers(data))
+    }
+    getUsers()
+  }, [])
+
+  return (
+    <ul className="users-list">
+      {
+        users.map(user => (
+          <li key={user.id}>
+            <Card user={user} />
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+export default UserList
