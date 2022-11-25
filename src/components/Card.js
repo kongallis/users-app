e28119e3ba2e354724329c75317ee86a13aceb7f
Konstantinos Gallis <kongallis@gmail.com>
2022-11-25 19:54:54 +0200

    Install react-lazy-load-image-component for lazy loading

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,16 @@
 import Details from "./Details"
+import { LazyLoadImage } from "react-lazy-load-image-component"
+import 'react-lazy-load-image-component/src/effects/blur.css'
 
 const Card = ({ user }) => {
   return (
     <article className="card">
-      <img
+      <LazyLoadImage
         src={user.avatar}
-        alt=""
-        width=""
-        height=""
-        loading="lazy"
+        alt={`${user.first_name} ${user.last_name} avatar`}
+        width={208}
+        height={208}
+        effect="blur"
       />
       <Details user={user} />
     </article>
