6f73a3e8baed10a256473edc41bb137a229e503e
Konstantinos Gallis <kongallis@gmail.com>
2022-11-25 17:56:59 +0200

    Create functionality for rendering the list of users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useState, useEffect } from "react";
+import Card from "./components/Card"
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const result = fetch('https://random-data-api.com/api/v2/users?size=5')
+        .then(response => response.json())
+        .then(data => setUsers(data))
+        .then(console.log(users))
+    }
+
+    getUsers();
+  }, []);
+
+  return (
+    <ul className="users-list">
+      {
+        users.map(user => <li key={user.id}><Card user={user} /></li>)
+      }
+    </ul>
+  )
 }
 
 export default App
